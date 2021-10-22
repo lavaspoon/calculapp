06a54be1951e697543fb2a30887a6fe0749a6506
lavaspoon <lavaspoon@naver.com>
2021-10-23 08:17:26 +0900

    Calculate 화면 UI 구성완료

---
+++ calculapp/calculapp/Controller/CalculateViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CalculateViewController.swift
+//  calculapp
+//
+//  Created by lavaspoon on 2021/10/23.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("CalculateViewController Load")
+    }
+    @IBAction func tapAddBtn(_ sender: UIBarButtonItem) {
+        
+    }
+    @IBAction func tapEditBtn(_ sender: UIBarButtonItem) {
+    }
+}
---
+++ calculapp/calculapp/Controller/ChartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChartViewController.swift
+//  calculapp
+//
+//  Created by lavaspoon on 2021/10/23.
+//
+
+import UIKit
+
+class ChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ChartViewController Load")
+    }
+    
+}
