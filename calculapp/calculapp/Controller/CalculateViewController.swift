643488a3be4f796204ba250e6e36032ba9aedb0c
lavaspoon <lavaspoon@naver.com>
2021-10-23 21:30:25 +0900

    Calculate 화면 체크박스 기능 구현 완료

--- calculapp/calculapp/Controller/CalculateViewController.swift
+++ calculapp/calculapp/Controller/CalculateViewController.swift
@@ -16,20 +16,17 @@ class CalculateViewController: UIViewController {
         super.viewDidLoad()
         print("CalculateViewController Load")
         self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
     
     @IBAction func tapAddBtn(_ sender: UIBarButtonItem) {
-        //alert 작업
         let alert = UIAlertController(title: "정산 목록 추가", message: nil, preferredStyle: .alert)
         let registBtn = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
             
             guard let title = alert.textFields?[0].text else { return }
-            guard let money = alert.textFields?[1].text else { return }
-            print("품목: \(title), 금액: \(money)")
-            
-            let form = Form(title: title, money: money)
+            guard let money = Int(alert.textFields?[1].text ?? "") else { return }
+            let form = Form(title: title, money: money, done: false)
             self?.forms.append(form)
-            print(form)
             self?.tableView.reloadData()
         })
         let cancleBtn = UIAlertAction(title: "취소", style: .default, handler: nil)
@@ -55,10 +52,23 @@ extension CalculateViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let form = self.forms[indexPath.row]
-        print(form)
         cell.textLabel?.text = form.title
         cell.detailTextLabel?.text = ("\(form.money) 원")
+        if form.done {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         return cell
     }
     
+}
+
+extension CalculateViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var form = self.forms[indexPath.row]
+        form.done = !form.done
+        self.forms[indexPath.row] = form
+        self.tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
 }
--- calculapp/calculapp/Model/Form.swift
+++ calculapp/calculapp/Model/Form.swift
@@ -9,5 +9,6 @@ import Foundation
 
 struct Form {
     var title : String
-    var money : String
+    var money : Int
+    var done : Bool
 }
